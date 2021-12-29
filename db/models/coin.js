f64dd5fce22a13f12b8f8cbe7cc48b1840dc682f
x-1337-x <g1thub_acc@protonmail.com>
2021-12-29 15:46:51 +0200

    coin and watchlist models

---
+++ db/models/coin.js
@@ -0,0 +1,38 @@
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class Coin extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Coin.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				validate: { notNull: false, notEmpty: true },
+			},
+			symbol: {
+				type: DataTypes.STRING,
+				validate: { notNull: false, notEmpty: true },
+			},
+			// contractAddress: {
+			// 	type: DataTypes.STRING,
+			// 	validate: { notNull: false, notEmpty: true },
+			// },
+			description: {
+				type: DataTypes.STRING,
+				validate: { notNull: false, notEmpty: true },
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Coin',
+		}
+	);
+	return Coin;
+};
