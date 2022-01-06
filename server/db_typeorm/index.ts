13b7e52881a52ec21a8f698f2c1dc56c82b585fe
Denys bigjey Izhetskyi <mail.bigjey@gmail.com>
2022-01-06 21:09:25 +0100

    fix

--- server/db_typeorm/index.ts
+++ server/db_typeorm/index.ts
@@ -2,7 +2,11 @@ import { ConnectionOptions, createConnection } from 'typeorm';
 import { DB_SETTINGS } from '../constants';
 
 export const setupDB = async () => {
-    const env = process.env.NODE_ENV as 'development' | 'test';
+    const env = (process.env.NODE_ENV || 'development') as
+        | 'development'
+        | 'test';
+
+    console.log({ env });
 
     await createConnection(DB_SETTINGS[env] as ConnectionOptions);
 };
